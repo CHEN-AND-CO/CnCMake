a029c9f6b28079afe225194a20a6ec45f68c6560
BlivionIaG <kev29lt@gmail.com>
2019-05-24 18:59:56 +0200

    Organized

--- classicMakefile.mk
+++ classicMakefile.mk
@@ -133,7 +133,7 @@ $(info Used compiler is now : $(CC))
 $(info )
 
 # Config override
-$(foreach line, $(CONFIG), $(info $(subst =, ,$(strip $(line)))))
+$(foreach line, $(CONFIG), $(if $(filter BINARY_NAME,$(subst =, ,$(strip $(line)))),$(info $(subst =, ,$(strip $(line)))),))
 
 # OS Detection
 ifeq ($(OS),Windows_NT)     					# Windows OS
